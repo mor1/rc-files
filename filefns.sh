437f3f3568d5ef46197d4a45986632c35d16ddc9
Richard Mortier <mort@cantab.net>
2010-01-08 11:06:53 +0000

    .

--- filefns.sh
+++ filefns.sh
@@ -1,6 +1,22 @@
-######################################################################
 #
-# $Id: filefns.sh,v 1.10 2009/05/20 09:42:40 mort Exp mort $
+# Some file related bash shell functions
+#
+# Copyright (C) 2000 Richard Mortier <mort@cantab.net>.  All Rights
+# Reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+# USA.
 
 #
 # recursive grep; note need for quotes, and the existence of GNU grep
