f191912f1a17a77d3c2a8eed08ac34bdaeed34b2
Richard Mortier <mort@cantab.net>
2013-11-23 22:09:15 +0000

    move container dir for fs links

--- hosts.sh
+++ hosts.sh
@@ -29,23 +29,47 @@ devbox () {
 hw2 () {
   slogin -X root@ubuntu.local
 }
+
+stmwww() {
+  slogin -p 722 -X stthnorg@stthomasmorewollaton.org.uk
+}
   
+mediapc () {
+  slogin -X root@mediapc.home
+}
+
 #
 # remote fs
 #
 
 marianfs () {
-  sshfs marian.cs.nott.ac.uk:/$1 ~/m/marian
+  sshfs marian.cs.nott.ac.uk:/$1 ~/l/marian
 }
 
 severnfs () {
-  sshfs severn.cs.nott.ac.uk:/$1 ~/m/severn
+  sshfs severn.cs.nott.ac.uk:/$1 ~/l/severn
 }
 
 stratusfs () {
-  sshfs stratus.cs.nott.ac.uk:/$1 ~/m/stratus
+  sshfs stratus.cs.nott.ac.uk:/$1 ~/l/stratus
 }
 
 hw2fs() {
-  sshfs ubuntu.local:/$1 ~/m/hw2
+  sshfs ubuntu.local:/$1 ~/l/hw2
+}
+
+stmwwwfs() {
+  sshfs -p 722 stthnorg@stthomasmorewollaton.org.uk: ~/l/stm-www
+}
+
+vagrantfs() {
+  sshfs -p 2222 vagrant@localhost: ~/l/vagrant
+}
+
+mediapcfs() {
+  sshfs root@mediapc.home:/$1 ~/l/mediapc
+}
+
+cuclfs () {
+  sshfs rmm1002@slogin-serv.cl.cam.ac.uk:$1 ~/l/cucl
 }
