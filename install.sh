4167da7aaa221418ce08afefed99cec8ea52a366
Richard Mortier <mort@cantab.net>
2014-01-31 11:20:20 +0000

    [install] whitespace; add to ignore directories

--- install.sh
+++ install.sh
@@ -6,7 +6,8 @@ for f in ${INSTALL_DIR}/*; do
   bf=$(basename $f)
   case "$bf" in
   "floatlg.jpg" | "install.sh" | \
-    "push-env.sh" | "solarized-dark-mort.itermcolors" ) 
+    "push-env.sh" | "solarized-dark-mort.itermcolors" | \
+    "emacs.dead" | "macbook-uk.layout" )
     ;;
   * )
     [ -L ~/.$bf ] && rm -f ~/.$bf || true
