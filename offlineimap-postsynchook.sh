058772abe921a62d8ad4cb9e88063eaf940f3763
Richard Mortier <mort@cantab.net>
2015-10-31 14:44:32 +0000

    offlineimap: prefer `mu` to `notmuch`; sort addrs

--- offlineimap-postsynchook.sh
+++ offlineimap-postsynchook.sh
@@ -18,5 +18,23 @@ set -e
 
 echo $(date) offlineimap: post-sync
 export PATH=/usr/local/bin:$PATH
-notmuch new
+
+if [ "$1" == "--index" ] ; then
+    # notmuch new
+    mu index --quiet --maildir=~/me/footprint/mail      \
+       --my-address=mort@cantab.net                     \
+       --my-address=mort@live.co.uk                     \
+       --my-address=mort@microsoft.com                  \
+       --my-address=mort@sprintlabs.com                 \
+       --my-address=mort@vipadia.com                    \
+       --my-address=richard.mortier@cl.cam.ac.uk        \
+       --my-address=richard.mortier@gmail.com           \
+       --my-address=richard.mortier@hotmail.com         \
+       --my-address=richard.mortier@nottingham.ac.uk    \
+       --my-address=richard.mortier@unikernel.com       \
+       --my-address=rmm1002@cam.ac.uk                   \
+       --my-address=rmm1002@hermes.cam.ac.uk            \
+       --my-address=rmm@cs.nott.ac.uk
+fi
+
 logger -t offlineimap -p mail.info "Sync of mail account completed"
