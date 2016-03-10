0efe7f1f346137f2941533a4ecdc1d9b28b02eaa
Richard Mortier <mort@cantab.net>
2016-03-10 10:15:38 +0000

    sh: add some docker shell fns

---
+++ dockerfns.sh
@@ -0,0 +1,25 @@
+#
+# Some Docker related bash shell functions
+#
+# Copyright (C) 2016 Richard Mortier <mort@cantab.net>. All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License version 2 as published by the Free
+# Software Foundation
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+# Place - Suite 330, Boston, MA 02111-1307, USA.
+
+function docker-kill {
+    docker ps --all --quiet | xargs docker stop -t 2 --quiet
+    docker ps --all --quiet | xargs docker rm
+}
+
+function docker-clean {
+    docker images | grep "<none>" | xargs docker rmi
+}
