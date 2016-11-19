801e9321beae4566abbc1acec99b21f85fc38c3a
Richard Mortier <mort@cantab.net>
2016-11-19 13:44:35 +0000

    docker: tweaks

--- dockerfns.sh
+++ dockerfns.sh
@@ -26,25 +26,94 @@ function docker-clean {
 }
 
 function d {
-    docker run -ti --rm -v $(pwd -P):/cwd -w /cwd "$@"
+#    docker pull "$1" > /dev/null
+    docker run -ti --rm -v "$(pwd -P)":/cwd -w /cwd $DARGS "$@"
 }
 
-function linux {
-    d alpine sh -c "$*"
+## base distros
+
+function alpine {
+    if [ -z "$@" ] ; then
+        d alpine /bin/sh
+    else
+        d alpine /bin/sh -c "$*"
+    fi
+}
+
+function debian {
+    if [ -z "$@" ] ; then
+        d debian:latest bash
+    else
+        d debian:latest bash -c "$*"
+    fi
+}
+
+function ubuntu {
+    if [ -z "$@" ] ; then
+        d ubuntu:latest bash
+    else
+        d ubuntu:latest bash -c "$*"
+    fi
+}
+
+## containerised apps
+
+function casperjs {
+    d mor1/casperjs:latest "$@"
 }
 
 function coffee {
     d mor1/alpine-coffeescript:latest "$@"
 }
 
+function dos2unix {
+    d mor1/alpine-dos2unix:latest "$@"
+}
+
+function gawk {
+    d mor1/alpine-gawk:latest "$@"
+}
+
 function jekyll {
-    d mor1/alpine-jekyll:latest "$@"
+    d mor1/jekyll:latest "$@"
+}
+
+function jhead {
+    d mor1/alpine-jhead:latest "$@"
+}
+
+function jq {
+    d mor1/alpine-jq:latest "$@"
+}
+
+function lessc {
+    d mor1/lessc:latest "$@"
+}
+
+function lilypond {
+    d markauskas/lilypond lilypond "$@"
+}
+
+function lynx {
+    d mor1/alpine-lynx "$@"
+}
+
+function pandoc {
+    d -v ~/.pandoc:/root/.pandoc mor1/pandoc:latest "$@"
+}
+
+function phantomjs {
+    d mor1/phantomjs:latest "$@"
+}
+
+function poly {
+    d mor1/polyml:latest "$@"
 }
 
 function python3 {
     d mor1/alpine-python3:latest "$@"
 }
 
-function casperjs {
-    d casperjs-local casperjs "$@"
+function unzip {
+    d mor1/alpine-unzip:latest "$@"
 }
