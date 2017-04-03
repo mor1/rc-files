4851d8b6cb4a1d89dd707cba9173a051fce088bf
Richard Mortier <mort@cantab.net>
2017-04-03 22:01:19 +0100

    install: ssh

--- install.sh
+++ install.sh
@@ -29,8 +29,8 @@ pushd $INDIR
 
     [ -s $file ] && mv $file ssh
 done
-[ -d ~/.ssh -a ! -L ~/.ssh ] && rmdir ~/.ssh
-ln -s $INDIR/ssh ~/.ssh
+[ -d ~/.ssh -a ! -L ~/.ssh ] \
+    && ( rmdir ~/.ssh ; ln -s $INDIR/ssh ~/.ssh )
 
 ## i value consistency in my environments. so what?
 rm -f ~/.bashrc
