e7a66d2e4ea1d91c58a0bde26768731ac247326b
Richard Mortier <mort@cantab.net>
2017-10-30 22:21:12 +0000

    env: filefns tidy-ups and updates

--- filefns.sh
+++ filefns.sh
@@ -21,29 +21,29 @@
 #
 
 trawl () {
-    find . \(                                                       \
-         -name '*.[chsSyli]'                                        \
-         -or -name 'Make*'                                          \
-         -or -name 'Dockerfile'                                     \
-         -or -name 'README*'                                        \
-         -or -name '*.bib'                                          \
-         -or -name '*.cc' -or -name '*.hh'                          \
-         -or -name '*.xml' -or -name '*.html' -or -name '*.css'     \
-         -or -name '*.el'                                           \
-         -or -name '*.fs' -or -name '*.fs[xi]'                      \
-         -or -name '*.go'                                           \
-         -or -name '*.inc'                                          \
-         -or -name '*.less'                                         \
-         -or -name '*.md'                                           \
-         -or -name '*.nix'                                          \
-         -or -name '*.php'                                          \
-         -or -name '*.py'                                           \
-         -or -name '*.tex'                                          \
-         -or \( \( -name '*.ml' -or -name '*.ml[yil]' \)            \
-               -not -path '*/_build/*'                              \
-               -not -name 'setup.ml' -not -name 'myocamlbuild.ml'   \
-               \)                                                   \
-         \) -print0 | xargs -0 grep -EHns "$@"
+  find . \(                                                       \
+       -name '*.[chsSyli]'                                        \
+       -or -name 'Make*'                                          \
+       -or -name 'Dockerfile'                                     \
+       -or -name 'README*'                                        \
+       -or -name '*.bib'                                          \
+       -or -name '*.cc' -or -name '*.hh'                          \
+       -or -name '*.xml' -or -name '*.html' -or -name '*.css'     \
+       -or -name '*.el'                                           \
+       -or -name '*.fs' -or -name '*.fs[xi]'                      \
+       -or -name '*.go'                                           \
+       -or -name '*.inc'                                          \
+       -or -name '*.less'                                         \
+       -or -name '*.md'                                           \
+       -or -name '*.nix'                                          \
+       -or -name '*.php'                                          \
+       -or -name '*.py'                                           \
+       -or -name '*.tex'                                          \
+       -or \( \( -name '*.ml' -or -name '*.ml[yil]' \)            \
+       -not -path '*/_build/*'                              \
+       -not -name 'setup.ml' -not -name 'myocamlbuild.ml'   \
+       \)                                                   \
+       \) -print0 | xargs -0 grep -EHns "$@"
 }
 
 #
@@ -51,42 +51,42 @@ trawl () {
 #
 
 symt () {
-    local argsdone=0
-    local verbose=0
-    local quiet=0
+  local argsdone=0
+  local verbose=0
+  local quiet=0
 
-    while [ $argsdone != 1 ]; do
-        case $1 in
-            -q) quiet=1 ;;
-            -v) verbose=1 ;;
-            *)  argsdone=1 ;;
-        esac
+  while [ $argsdone != 1 ]; do
+    case $1 in
+      -q) quiet=1 ;;
+      -v) verbose=1 ;;
+      *)  argsdone=1 ;;
+    esac
 
-        args="$args $1"
-        if [ $argsdone = 0 ]; then
-            shift
+    args="$args $1"
+    if [ $argsdone = 0 ]; then
+      shift
+    fi
+  done
+
+  for f in "$@"; do
+    if [ -L "$f" ] ; then
+      if [ ! -r $f ]; then
+        echo "$f: dangling"
+        retval=1
+      else
+        if [ $quiet = 0 ]; then
+          echo "$f: ok"
+          retval=0
         fi
-    done
-
-    for f in "$@"; do
-        if [ -L "$f" ] ; then
-            if [ ! -r $f ]; then
-                echo "$f: dangling"
-                retval=1
-            else
-                if [ $quiet = 0 ]; then
-                    echo "$f: ok"
-                    retval=0
-                fi
-            fi
-        else
-            if [ $verbose = 1 ]; then
-                echo "$f: not a symlink"
-                retval=0
-            fi
-        fi
-    done
-    return $retval
+      fi
+    else
+      if [ $verbose = 1 ]; then
+        echo "$f: not a symlink"
+        retval=0
+      fi
+    fi
+  done
+  return $retval
 }
 
 #
@@ -94,22 +94,22 @@ symt () {
 #
 
 rfc () {
-    if [ $# = 1 ]; then
-        if [ "$1" = "-index" ]; then
-            curl -o ~/docs/rfcs/rfc${1}.txt \
-                 http://www.rfc-editor.org/rfc/rfc${1}.txt
-        elif [ ! -s ~/docs/rfcs/rfc${1}.txt ] ; then
-            curl -o ~/docs/rfcs/rfc${1}.txt \
-                 http://www.rfc-editor.org/rfc/rfc${1}.txt
-        fi
-        less -i ~/docs/rfcs/rfc${1}.txt
-    else
-        case $1 in
-            -print|-p) a2ps --highlight-level=normal --columns=4 \
-                            --row=1 -l82 --interpret=no \
-                            ~/docs/rfcs/rfc${2}.txt ;;
-        esac
+  if [ $# = 1 ]; then
+    if [ "$1" = "-index" ]; then
+      curl -o ~/docs/rfcs/rfc${1}.txt \
+           http://www.rfc-editor.org/rfc/rfc${1}.txt
+    elif [ ! -s ~/docs/rfcs/rfc${1}.txt ] ; then
+      curl -o ~/docs/rfcs/rfc${1}.txt \
+           http://www.rfc-editor.org/rfc/rfc${1}.txt
     fi
+    less -i ~/docs/rfcs/rfc${1}.txt
+  else
+    case $1 in
+      -print|-p) a2ps --highlight-level=normal --columns=4 \
+                      --row=1 -l82 --interpret=no \
+                      ~/docs/rfcs/rfc${2}.txt ;;
+    esac
+  fi
 }
 
 #
@@ -122,15 +122,15 @@ PANDOC_BASE='
 PANDOC_MD="$PANDOC_BASE --number-sections -Vgeometry=margin=2cm"
 
 md2tex () {
-    $PANDOC_MD -o ${1%.md}.tex $@
+  $PANDOC_MD -o ${1%.md}.tex $@
 }
 
 md2docx () {
-    $PANDOC_MD -o ${1%.md}.docx $@
+  $PANDOC_MD -o ${1%.md}.docx $@
 }
 
 md2pdf () {
-    $PANDOC_MD -o ${1%.md}.pdf $@
+  $PANDOC_MD -o ${1%.md}.pdf $@
 }
 
 PANDOC_LETTER="\
@@ -139,15 +139,15 @@ PANDOC_LETTER="\
     "
 
 letter2tex () {
-    $PANDOC_LETTER -o ${1%.md}.tex $@
+  $PANDOC_LETTER -o ${1%.md}.tex $@
 }
 
 letter2doc () {
-    $PANDOC_LETTER -o ${1%.md}.docx $@
+  $PANDOC_LETTER -o ${1%.md}.docx $@
 }
 
 letter2pdf () {
-    $PANDOC_LETTER -o ${1%.md}.pdf $@
+  $PANDOC_LETTER -o ${1%.md}.pdf $@
 }
 
 
@@ -159,52 +159,52 @@ letter2pdf () {
 #
 
 e () {
-    SERVER_SOCK=/tmp/emacs-$USER/server
-    [ -S $SERVER_SOCK ] && \
-        emacsclient -n -s $SERVER_SOCK "$@" || \
-            { /Applications/Emacs.app/Contents/MacOS/Emacs "$@" & }
+  SERVER_SOCK=/tmp/emacs-$USER/server
+  [ -S $SERVER_SOCK ] && \
+    emacsclient -n -s $SERVER_SOCK "$@" || \
+      { /Applications/Emacs.app/Contents/MacOS/Emacs "$@" & }
 }
 
 function emacspkg-update {
-    ( cd ~/rc-files/emacs.d/elpa &&
-          git rm "$1-*" &&
-          git add "$1-*" &&
-          git commit -m "emacs: update \`$1\`"
-    )
+  ( cd ~/rc-files/emacs.d/elpa &&
+      git rm "$1-*" &&
+      git add "$1-*" &&
+      git commit -m "emacs: update \`$1\`"
+  )
 }
 
 function emacspkg-add {
-    ( cd ~/rc-files/emacs.d/elpa &&
-          git add "$1-*" &&
-          git commit -m "emacs: add \`$1\`"
-    )
+  ( cd ~/rc-files/emacs.d/elpa &&
+      git add "$1-*" &&
+      git commit -m "emacs: add \`$1\`"
+  )
 }
 
 function emacspkg-rm {
-    ( cd ~/rc-files/emacs.d/elpa &&
-          git rm "$1-*" &&
-          git commit -m "emacs: remove \`$1\`"
-    )
+  ( cd ~/rc-files/emacs.d/elpa &&
+      git rm "$1-*" &&
+      git commit -m "emacs: remove \`$1\`"
+  )
 }
 
 function emacspkgs-commit-all {
-    pushd ~/rc-files
-    PKGS=$(git status -s | grep -E "^   emacs.d/.*$" | cut -f 3 -d "/" | uniq)
-    for n in $PKGS; do
-        emacspkg-update ${n%-*}
-    done
-    PKGS=$(git status -s | grep -E "^[?][?] emacs.d/.*$" | cut -f 3 -d "/" | uniq)
-    for n in $PKGS; do
-        emacspkg-add ${n%-*}
-    done
-    PKGS=$(git status -s | grep -E "^ D emacs.d/.*$" | cut -f 3 -d "/" | uniq)
-    for n in $PKGS; do
-        emacspkg-rm ${n%-*}
-    done
-    popd
-    ( cd ~/rc-files/emacs.d/elpa/archives \
-          && git commit -m "emacs: update archives" */archive-contents*
-    )
+  pushd ~/rc-files/emacs.d
+  PKGS=$(git status -s | grep -E "^   elpa/.*$" | cut -f 2 -d "/" | uniq)
+  for n in $PKGS; do
+    emacspkg-update ${n%-*}
+  done
+  PKGS=$(git status -s | grep -E "^[?][?] elpa/.*/$" | cut -f 2 -d "/" | uniq)
+  for n in $PKGS; do
+    emacspkg-add ${n%-*}
+  done
+  PKGS=$(git status -s | grep -E "^ D elpa/.*$" | cut -f 2 -d "/" | uniq)
+  for n in $PKGS; do
+    emacspkg-rm ${n%-*}
+  done
+  ( git add elpa/archives/ &&
+      git commit -m "emacs: update archives" elpa/archives/
+  )
+  popd
 }
 
 #
@@ -212,21 +212,21 @@ function emacspkgs-commit-all {
 #
 
 abspath () {
-    p=$1
-    pushd . > /dev/null
-    if [ -d "$1" ]; then
-        cd "$1"
-        dirs -l +0
+  p=$1
+  pushd . > /dev/null
+  if [ -d "$1" ]; then
+    cd "$1"
+    dirs -l +0
+  else
+    cd $(dirname $(dirname $(which $p))/$(readlink $p))
+    cur_dir=`dirs -l +0`
+    if [ "$cur_dir" == "/" ]; then
+      echo "$cur_dir`basename \"$1\"`"
     else
-        cd $(dirname $(dirname $(which $p))/$(readlink $p))
-        cur_dir=`dirs -l +0`
-        if [ "$cur_dir" == "/" ]; then
-            echo "$cur_dir`basename \"$1\"`"
-        else
-            echo "$cur_dir/`basename \"$1\"`"
-        fi
+      echo "$cur_dir/`basename \"$1\"`"
     fi
-    popd > /dev/null
+  fi
+  popd > /dev/null
 }
 
 #
@@ -234,22 +234,22 @@ abspath () {
 #
 
 function update-all {
-    brew update && brew upgrade --cleanup \
-        && brew cask reinstall $(brew cask outdated) && brew cask cleanup
-    opam update -y -u
-    rm -f ~/.profile
+  brew update && brew upgrade --cleanup \
+    && brew cask reinstall $(brew cask outdated) && brew cask cleanup
+  opam update -y -u
+  rm -f ~/.profile
 }
 
 function opam-switch {
-    opam switch "$@"
-    use-ocaml
+  opam switch "$@"
+  use-ocaml
 }
 
 function use-ocaml {
-    unset CAML_LD_LIBRARY_PATH
-    if [ \! -z $(which opam) ]; then
-        eval $(opam config env)
-    fi
+  unset CAML_LD_LIBRARY_PATH
+  if [ \! -z $(which opam) ]; then
+    eval $(opam config env)
+  fi
 }
 
 #
@@ -257,11 +257,11 @@ function use-ocaml {
 #
 
 function git-cloner {
-    git clone "$1"
-    B=$(basename -s .git "$1")
-    cd $B
-    git remote add upstream "$2/$B.git"
-    git pull upstream master
+  git clone "$1"
+  B=$(basename -s .git "$1")
+  cd $B
+  git remote add upstream "$2/$B.git"
+  git pull upstream master
 }
 
 #
@@ -269,8 +269,8 @@ function git-cloner {
 #
 
 function ddstatus {
-    PID=$(ps -ax | grep "[0-9] dd" | tr -s ' ' | cut -f 1 -d ' ' | xargs)
-    sudo kill -INFO $PID
+  PID=$(ps -ax | grep "[0-9] dd" | tr -s ' ' | cut -f 1 -d ' ' | xargs)
+  sudo kill -INFO $PID
 }
 
 #
@@ -278,20 +278,20 @@ function ddstatus {
 #
 
 function photos-rename {
-    jhead -exonly -ft -n%Y%m%d-%H%M%S-%03i "$@"
+  jhead -exonly -ft -n%Y%m%d-%H%M%S-%03i "$@"
 }
 
 function photos-subdir {
-    for n in *.jpg ; do
-        d=${n%%-*}-- && ( [ \! -d $d ] && mkdir $d || true ) && mv $n $d
-    done
-    # for n in *.jpg ; do
-    #    mv $n $(echo $n | cut -b1-4)-$(echo $n | cut -b5-6)-$(echo $n | cut -b7-8)--
-    # done
+  for n in *.jpg ; do
+    d=${n%%-*}-- && ( [ \! -d $d ] && mkdir $d || true ) && mv $n $d
+  done
+  # for n in *.jpg ; do
+  #    mv $n $(echo $n | cut -b1-4)-$(echo $n | cut -b5-6)-$(echo $n | cut -b7-8)--
+  # done
 }
 
 function photos-mirror {
-    rsync -av --delete ./pictures/ ~/l/mediapc/storage/pictures
+  rsync -av --delete ./pictures/ ~/l/mediapc/storage/pictures
 }
 
 #
@@ -299,14 +299,14 @@ function photos-mirror {
 #
 
 function v6-off {
-    while read -r line ; do
-        echo "XXX: $line"
-    done <<< $(networksetup -listallnetworkservices | tail -n+2)
+  while read -r line ; do
+    echo "XXX: $line"
+  done <<< $(networksetup -listallnetworkservices | tail -n+2)
 }
 
 function v6-on {
-    for i in "$(networksetup -listallnetworkservices | tail -n+2)"; do
-        echo "sudo networksetup -setv6automatic \"$i\" || true"
-        sudo networksetup -setv6automatic "$i" || true
-    done
+  for i in "$(networksetup -listallnetworkservices | tail -n+2)"; do
+    echo "sudo networksetup -setv6automatic \"$i\" || true"
+    sudo networksetup -setv6automatic "$i" || true
+  done
 }
