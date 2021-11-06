22005daeb2df81fc2079f11139d9b69fcb05e736
Richard Mortier <mort@cantab.net>
2021-11-06 11:05:49 +0000

    offlineimap: record how to init
    Signed-off-by: Richard Mortier <mort@cantab.net>

--- offlineimap-postsynchook.sh
+++ offlineimap-postsynchook.sh
@@ -26,3 +26,17 @@ if [ "$1" == "--index" ] ; then
 fi
 
 logger -t offlineimap -p mail.info "Sync of mail account completed"
+
+# mu init --maildir=~/me/footprint/mail --my-address=mort@cantab.net
+# --my-address=mort@live.co.uk --my-address=mort@microsoft.com
+# --my-address=mort@sprintlabs.com --my-address=mort@vipadia.com
+# --my-address=pszrmm@exmail.nottingham.ac.uk
+# --my-address=pszrmm@nottingham.ac.uk
+# --my-address=richard.mortier@cdbb.cam.ac.uk
+# --my-address=richard.mortier@cl.cam.ac.uk
+# --my-address=richard.mortier@docker.com --my-address=richard.mortier@gmail.com
+# --my-address=richard.mortier@hotmail.com
+# --my-address=richard.mortier@nottingham.ac.uk
+# --my-address=richard.mortier@unikernel.com --my-address=rmm1002@cam.ac.uk
+# --my-address=rmm1002@hermes.cam.ac.uk --my-address=rmm1002@srcf.net
+# --my-address=rmm@cs.nott.ac.uk
