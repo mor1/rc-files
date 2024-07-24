8419418945ab2d3848895bf3f0df6b89104c14f4
Richard Mortier <mort@cantab.net>
2024-07-24 14:53:16 +0100

    nixos: tweaking pipewire sound setup

--- nixos/systems/greyjay/default.nix
+++ nixos/systems/greyjay/default.nix
@@ -130,13 +130,42 @@ in
   console.keyMap = "uk";
 
   # audio
-  sound.mediaKeys.enable = true;
   security.rtkit.enable = true;
   services.pipewire = {
-    enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
+    enable = true;
+    jack.enable = true;
     pulse.enable = true;
+
+    extraConfig = {
+      pipewire."92-low-latency" = {
+        context.properties = {
+          default.clock.rate = 48000;
+          default.clock.quantum = 32;
+          default.clock.min-quantum = 32;
+          default.clock.max-quantum = 32;
+        };
+      };
+      pipewire-pulse."92-low-latency" = {
+        context.modules = [
+          {
+            name = "libpipewire-module-protocol-pulse";
+            args = {
+              pulse.min.req = "32/48000";
+              pulse.default.req = "32/48000";
+              pulse.max.req = "32/48000";
+              pulse.min.quantum = "32/48000";
+              pulse.max.quantum = "32/48000";
+            };
+          }
+        ];
+        stream.properties = {
+          node.latency = "32/48000";
+          resample.quality = 1;
+        };
+      };
+    };
   };
 
   # system services
