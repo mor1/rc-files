1a5ba83f046709866c423aa1bc206d519a28640d
Richard Mortier <mort@cantab.net>
2024-11-14 17:13:24 +0000

    nixos: improve sync of my org files

--- nixos/home-manager/greyjay/default.nix
+++ nixos/home-manager/greyjay/default.nix
@@ -7,7 +7,7 @@
 # pushd ~/rc-files/nixos ; nixfmt configuration.nix flake.nix home-manager/default.nix ; popd
 # pushd ~/u/src/dl-solar ; ./dl-solar yesterday ; popd
 
-{ ... }:
+{ pkgs, lib, ... }:
 let
   username = "mort";
 in
@@ -27,6 +27,46 @@ in
   };
 
   programs.home-manager.enable = true;
+
+  systemd.user.services."org-sync" = {
+    Unit = {
+      Description = "org sync";
+    };
+    Service = {
+      Type = "oneshot";
+      ExecStart = toString (
+        pkgs.writeShellScript "org-sync.sh" ''
+          set -eou pipefail
+
+          PATH=$PATH:${lib.makeBinPath [ pkgs.coreutils ]}
+
+          EXPORTBASE='/home/mort/Dropbox/calendar'
+          EXPORT="richard-incoming.org richard.org richard-tripit.org"
+
+          IMPORTBASE='/home/mort/Dropbox/people/family.org'
+          IMPORT="angela.org david.org eleanor.org william.org \
+            birthdays.org car.org home.org solemnities.org"
+
+          for f in $EXPORT; do
+            cp -a "$EXPORTBASE/$f" "$IMPORTBASE/$f"
+          done
+
+          for f in $IMPORT; do
+            cp -a "$IMPORTBASE/$f" "$EXPORTBASE/$f"
+          done
+        ''
+      );
+    };
+    Install.WantedBy = [ "default.target" ];
+  };
+
+  systemd.user.timers."org-sync" = {
+    Unit.Description = "org sync scheduler";
+    Timer = {
+      OnUnitActiveSec = "2m";
+    };
+    Install.WantedBy = [ "timers.target" ];
+  };
 
   home.stateVersion = "24.05";
 }
