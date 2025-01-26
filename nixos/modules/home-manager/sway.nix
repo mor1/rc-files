3f0c4bd9179305a9ac1bc9f5b42edc4cd2321c2b
Richard Mortier <mort@cantab.net>
2025-01-26 18:27:29 +0000

    sway: use function keys not just special keys

--- nixos/modules/home-manager/sway.nix
+++ nixos/modules/home-manager/sway.nix
@@ -193,6 +193,20 @@ in
             f12 = "exec rhythmbox-client --next";
           in
           lib.mkOptionDefault {
+            ## bare function keys
+            "F1" = f1;
+            "F2" = f2;
+            "F3" = f3;
+            "F4" = f4;
+            "F5" = f5;
+            "F6" = f6;
+            "F7" = f7;
+            "F8" = f8;
+            "F9" = f9;
+            "F10" = f10;
+            "F11" = f11;
+            "F12" = f12;
+
             ## thinkpad keyboard
             "XF86AudioMute" = f1;
             "XF86AudioLowerVolume" = f2;
