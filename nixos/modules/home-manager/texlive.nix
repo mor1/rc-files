5030e47d6538574a6afd5dfe53b0c960a9671e8b
Richard Mortier <mort@cantab.net>
2025-03-06 21:14:52 +0000

    texlive: be selective about packages

--- nixos/modules/home-manager/texlive.nix
+++ nixos/modules/home-manager/texlive.nix
@@ -1,6 +1,48 @@
 { pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    texlive.combined.scheme-full # latex installation
-  ];
+  home.packages =
+    let
+      texlive = (
+        pkgs.texlive.combine {
+          inherit (pkgs.texlive)
+            scheme-basic # scheme-medium
+
+            acmart
+            algorithm2e
+            amsmath
+            biblatex
+            booktabs
+            caption
+            comment
+            csquotes
+            environ
+            etoolbox
+            everyshi
+            float
+            fontspec
+            hyperref
+            hyperxmp
+            ifmtarg
+            latexmk
+            luacode
+            luatexbase
+            microtype
+            ncctools
+            preprint
+            textcase
+            textpos
+            totpages
+            ulem
+            xcolor
+            xkeyval
+            xstring
+            ;
+        }
+      );
+    in
+    with pkgs;
+    [
+      biber
+      texlive
+    ];
 }
