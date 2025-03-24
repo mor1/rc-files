a53c2093609f3986e855913a163541c792276abf
Richard Mortier <mort@cantab.net>
2025-03-24 15:34:11 +0000

    nixos: add more texlive packages for ACM papers

--- nixos/modules/home-manager/texlive.nix
+++ nixos/modules/home-manager/texlive.nix
@@ -6,33 +6,52 @@
         pkgs.texlive.combine {
           inherit (pkgs.texlive)
             scheme-basic # scheme-medium
+            xetex
 
             acmart
             algorithm2e
+            algorithmicx
+            algorithms
+            algpseudocodex
             amsmath
             biblatex
             booktabs
+            breqn
             caption
+            cmap
             comment
             csquotes
+            enumitem
             environ
             etoolbox
             everyshi
+            fifo-stack
             float
             fontspec
             hyperref
             hyperxmp
             ifmtarg
+            ifoddpage
             latexmk
             luacode
             luatexbase
             microtype
+            multirow
             ncctools
+            pgf
             preprint
+            relsize
+            setspace
+            subfigure
+            tabto-ltx
             textcase
             textpos
+            tikzmark
+            todonotes
+            totcount
             totpages
             ulem
+            varwidth
             xcolor
             xkeyval
             xstring
