0276a7359b9d74cb6c37a7d58a47b81425016409
Richard Mortier <mort@cantab.net>
2025-05-13 11:57:51 +0100

    nixos: add `binsider` TUI for binary exploration

--- nixos/modules/home-manager/tui.nix
+++ nixos/modules/home-manager/tui.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
+    binsider # analyse and edit ELF binaries
     fzf # fuzzy file finder; desired by yazi
     yazi # file manager
     zoxide # smarter cd; desired by yazi
