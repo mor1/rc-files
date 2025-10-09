c6af79c438ebddaa725fe7af72ceb888e5ee91a2
Richard Mortier <mort@cantab.net>
2025-10-09 22:53:27 +0100

    nixos: `eglot` and `tree-sitter` plus grammars for emacs

--- nixos/modules/home-manager/editors/emacs.nix
+++ nixos/modules/home-manager/editors/emacs.nix
@@ -1,9 +1,31 @@
 { pkgs, ... }:
 {
+  home.packages = with pkgs; [
+    emacs-lsp-booster
+  ];
+
   programs.emacs = {
     enable = true;
     package = pkgs.emacs-gtk;
-    extraPackages = (epkgs: [ epkgs.mu4e ]);
+    extraPackages =
+      epkgs: with epkgs; ([
+        corfu
+        jinx
+        mu4e
+        nerd-icons-corfu
+        pretty-sha-path
+        tree-sitter-langs
+        (treesit-grammars.with-grammars (p: [
+          p.tree-sitter-bash
+          p.tree-sitter-elisp
+          p.tree-sitter-nix
+          p.tree-sitter-ocaml
+          p.tree-sitter-python
+          p.tree-sitter-rust
+          p.tree-sitter-toml
+          p.tree-sitter-yaml
+        ]))
+      ]);
   };
   services.emacs.enable = true;
 }
