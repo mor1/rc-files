c13ccf4cc9ac09da89b25ce35c0e5bfa57d419a0
Richard Mortier <mort@cantab.net>
2026-01-02 17:59:48 +0000

    nixos: update

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -8,23 +8,10 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-    # cmake.url = "nixpkgs/e6f23dc08d3624daab7094b701aa3954923c6bbb";
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      home-manager,
-      # cmake,
-      ...
-    }@inputs:
-    let
-      system = "x86_64-linux";
-      # cmake_o = final: prev: {
-      #   cmake = inputs.cmake.pkg;
-      # };
-    in
+    { nixpkgs, home-manager, ... }@inputs:
     {
       nixosConfigurations = {
         greyjay = nixpkgs.lib.nixosSystem {
@@ -36,11 +23,6 @@
       homeConfigurations = {
         "mort@greyjay" = home-manager.lib.homeManagerConfiguration {
           pkgs = nixpkgs.legacyPackages.x86_64-linux;
-          # pkgs = import nixpkgs {
-          #   inherit inputs;
-          #   # overlays = [ cmake_o ];
-          # };
-
           extraSpecialArgs = { inherit inputs; };
           modules = [ ./home-manager/greyjay ];
         };
